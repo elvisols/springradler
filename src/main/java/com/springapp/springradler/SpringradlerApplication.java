ee43400dec823cc49ef0ea89df215b607266dadf
Elvis Orji <elvisouk@yahoo.com>
2021-07-13 00:52:35 +0100

    initial commit

---
+++ src/main/java/com/springapp/springradler/SpringradlerApplication.java
@@ -0,0 +1,14 @@
+package com.springapp.springradler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringradlerApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(SpringradlerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springapp/springradler/SpringradlerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springapp.springradler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringradlerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
